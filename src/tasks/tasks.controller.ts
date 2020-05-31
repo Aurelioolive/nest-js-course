00fa2d8709657f13786f4559c7e37afe1f4bc16a
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-05-31 17:20:24 -0300

    temporary get tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -22,6 +22,11 @@ import { TaskStatus } from './task-status.enum';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  @Get()
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto) {
+    return this.tasksService.getTasks(filterDto);
+  }
+
   @Get('/:id')
   getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
     return this.tasksService.getTaskById(id);
@@ -45,17 +50,4 @@ export class TasksController {
   ): Promise<Task> {
     return this.tasksService.updateTaskStatus(id, status);
   }
-
-  /*
-  @Get()
-  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-    if (Object.keys(filterDto).length) {
-      return this.tasksService.getTasksWithFilters(filterDto);
-    } else {
-      return this.tasksService.getAllTasks();
-    }
-  }
-
-  
-  */
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,6 +13,8 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
+  getTasks(filterDto: GetTasksFilterDto) {}
+
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
 
@@ -42,30 +44,4 @@ export class TasksService {
     await currentTask.save();
     return currentTask;
   }
-
-  /*
-
-  getAllTasks(): Task[] {
-    return this.tasks;
-  }
-
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-    let tasks = this.getAllTasks();
-
-    if (status) {
-      tasks = tasks.filter(task => task.status === status);
-    }
-
-    if (search) {
-      tasks = tasks.filter(
-        task =>
-          task.title.includes(search) || task.description.includes(search),
-      );
-    }
-
-    return tasks;
-  }  
-
-  */
 }
