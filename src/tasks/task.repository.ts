b1f9c08b69ad71784b3db6dfa354b9ff54dba8b1
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-05-31 20:27:39 -0300

    created method to search tasks

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -2,9 +2,32 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const query = this.createQueryBuilder('task');
+
+    if (status) {
+      console.log(`Searching Task where status = ${status}`);
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      console.log(`Searching Task that have something like "${search}"`);
+      query.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+
+    return tasks;
+  }
+
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const { title, description } = createTaskDto;
 
