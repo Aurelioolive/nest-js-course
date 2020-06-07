ea5a51f0263db935c5272ae1e25f4ea771caab78
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-07 05:36:53 -0300

    created signIn method

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -29,6 +29,19 @@ export class UserRepository extends Repository<User> {
     }
   }
 
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
+
   /**
    * Mix Password and Hash
    * @param password Password of user.
