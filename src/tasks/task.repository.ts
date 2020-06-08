9789c2c300a659a76dc45e59badb0736e5cd1ba2
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-08 02:15:44 -0300

    getting task of logged user

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -7,9 +7,12 @@ import { User } from '../auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
+
+    // just tasks of our logged user
+    query.where('task.userId = :userId', { userId: user.id });
 
     if (status) {
       console.log(`Searching Task where status = ${status}`);
