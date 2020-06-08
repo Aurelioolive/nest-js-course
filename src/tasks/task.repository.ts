aff48b8240f900396ea012a26aad418261cd878f
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-08 03:09:12 -0300

    added nestjs logging

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -4,9 +4,11 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { User } from '../auth/user.entity';
+import { Logger, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  private logger = new Logger('TaskRepository');
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -15,21 +17,29 @@ export class TaskRepository extends Repository<Task> {
     query.where('task.userId = :userId', { userId: user.id });
 
     if (status) {
-      console.log(`Searching Task where status = ${status}`);
       query.andWhere('task.status = :status', { status });
     }
 
     if (search) {
-      console.log(`Searching Task that have something like "${search}"`);
       query.andWhere(
         '(task.title LIKE :search OR task.description LIKE :search)',
         { search: `%${search}%` },
       );
     }
 
-    const tasks = await query.getMany();
+    try {
+      const tasks = await query.getMany();
 
-    return tasks;
+      return tasks;
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user ${
+          user.username
+        }, Filters: ${JSON.stringify(filterDto)}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
@@ -41,7 +51,17 @@ export class TaskRepository extends Repository<Task> {
     task.status = TaskStatus.OPEN;
     task.user = user;
 
-    await task.save();
+    try {
+      await task.save();
+    } catch (error) {
+      this.logger.error(
+        `Failed to create task for user ${
+          user.username
+        }. Data: ${JSON.stringify(createTaskDto)}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
 
     //detele user of return, not necessary
     delete task.user;
