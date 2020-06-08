e7a03b62e1b6d7c1229421d38ea9012a8c3ee41e
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-08 02:29:32 -0300

    get task by id just when the task belongs to user

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,8 +18,10 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({
+      where: { id, userId: user.id },
+    });
 
     if (!found) {
       throw new NotFoundException(`Task with ID ${id} not found`);
@@ -40,8 +42,12 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: number, newStatus: TaskStatus): Promise<Task> {
-    const currentTask = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: number,
+    newStatus: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const currentTask = await this.getTaskById(id, user);
     currentTask.status = newStatus;
 
     await currentTask.save();
