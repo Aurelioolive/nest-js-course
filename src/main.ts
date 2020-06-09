aa242451c2b4d8e9ebcfb07496b0c594dd0e349a
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-09 01:15:52 -0300

    setting port of application

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const logger = new Logger('Bootstrap');
-
-  const app = await NestFactory.create(AppModule);
-
   const serverConfig = config.get('server');
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || serverConfig.port;
 
-  await app.listen(serverConfig.port);
-  logger.log(`Application listening on port ${serverConfig.port}`);
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
+
 bootstrap();
