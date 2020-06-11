1eab8b495b8f6283ec60be8422914a83e1d55fa8
Aurélio Henrique <aurelio.oliveira@apinteractive.com.br>
2020-06-11 00:56:08 -0300

    enabled production mode

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ async function bootstrap() {
   if (process.env.NODE_ENV === 'development') {
     logger.log(`CORS are enabled`);
     app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from ${serverConfig.origin}`);
   }
 
   const port = process.env.PORT || serverConfig.port;
